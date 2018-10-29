20704df2e7635f360b2e3d79439a17f6ad6990c8
zhengxingtao <zheng_xigntao@126.com>
2018-10-29 17:22:12 +0800

    移除cat

--- component-web/src/main/java/com/jaagro/component/web/config/CatFilterConfigure.java
+++ component-web/src/main/java/com/jaagro/component/web/config/CatFilterConfigure.java
@@ -1,14 +1,9 @@
 package com.jaagro.component.web.config;
 
 import com.dianping.cat.servlet.CatFilter;
-import org.apache.ibatis.plugin.Interceptor;
-import org.mybatis.spring.SqlSessionFactoryBean;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import javax.sql.DataSource;
 
 /**
  * @author tony
@@ -25,14 +20,5 @@ public class CatFilterConfigure {
         registration.setName("cat-filter");
         registration.setOrder(1);
         return registration;
-    }
-
-    @Bean
-    public SqlSessionFactoryBean sqlSessionFactoryBean() {
-        SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
-        Interceptor[] plugins = {new CatMybatisPlugin()};
-        sqlSessionFactoryBean.setPlugins(plugins);
-        sqlSessionFactoryBean.setDataSource(DataSourceBuilder.create().build());
-        return sqlSessionFactoryBean;
     }
 }
